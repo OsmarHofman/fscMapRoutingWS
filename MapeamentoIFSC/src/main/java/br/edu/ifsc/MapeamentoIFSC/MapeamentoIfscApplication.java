9c43c597e21cdbb7c510ed771fc3638ac38ec6ea
aluno <aluno@debian>
2019-11-14 08:32:28 -0200

    Criando arquivos iniciais do projeto.

---
+++ MapeamentoIFSC/src/main/java/br/edu/ifsc/MapeamentoIFSC/MapeamentoIfscApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.MapeamentoIFSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoIfscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentoIfscApplication.class, args);
+	}
+
+}
---
+++ MapeamentoIFSC/src/test/java/br/edu/ifsc/MapeamentoIFSC/MapeamentoIfscApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.MapeamentoIFSC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapeamentoIfscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
