82f39eec0873ca29d369d36e4a90ca37ac49d629
Osmar José Hofman da Silva <osmarjhdas@hotmail.com>
2019-12-06 09:20:49 -0200

    Adicionando Requisição de Teste

--- src/main/java/br/edu/ifsc/connection/DataRoutes.java
+++ src/main/java/br/edu/ifsc/connection/DataRoutes.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,11 @@ public class DataRoutes {
 		acess = new DataAccess();
 		grafo = acess.pegarArquivo();
 		route = new Routing();
+	}
+
+	@GetMapping("/exemplo")
+	public String entrada() {
+		return "Fábrica";
 	}
 
 	@CrossOrigin
